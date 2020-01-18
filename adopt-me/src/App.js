a5c63a3e334e41cd74608fbe520a72f7a275d134
haitekie <haimanot.tekie@finn.no>
2020-01-18 16:40:10 +0100

    Pure React: Reusable Components

---
+++ adopt-me/src/App.js
@@ -0,0 +1,21 @@
+const Pet = () => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Luna"),
+        React.createElement("h2", {}, "Dog"),
+        React.createElement("h1", {}, "Havanese"),
+    ]);
+};
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1", {}, "Adopt Me!"),
+        [
+            React.createElement(Pet),
+            React.createElement(Pet),
+            React.createElement(Pet)
+        ]);
+    };
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"))
