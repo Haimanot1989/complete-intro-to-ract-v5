38488fcb76defe23fd28d91ae383151b9a626725
haitekie <haimanot.tekie@finn.no>
2020-06-09 07:06:15 +0200

    Async & Routing: Reach router

--- adopt-me/src/App.js
+++ adopt-me/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
+import { Router } from "@reach/router";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
---
+++ adopt-me/src/Details.js
@@ -0,0 +1,6 @@
+import React from "react";
+const Details = () => {
+  return <h1>Hello</h1>;
+};
+
+export default Details;
